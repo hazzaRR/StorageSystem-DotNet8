222cbe971da5075b02b2ae9fddb5e4ce96016b30
harryredman06@gmail.com <68497855+hazzaRR@users.noreply.github.com>
2024-05-21 16:17:06 +0100

    first commit

---
+++ StorageSystem/Controllers/StorageBinController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace StorageSystem.Controllers
+{
+    [Route("api/bin")]
+    [ApiController]
+    public class StorageBinController : ControllerBase
+    {
+
+        [HttpGet]
+        public IActionResult index()
+        {
+            return Ok("hello world");
+        }
+    }
+}
