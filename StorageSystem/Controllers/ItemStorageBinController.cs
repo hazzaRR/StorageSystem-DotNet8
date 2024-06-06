7ff691080b7ea34b87a006db8d4c0ed0d78bd883
harryredman06@gmail.com <68497855+hazzaRR@users.noreply.github.com>
2024-06-06 14:10:21 +0100

    added ItemStorageBin controller for adding and removing items from a storagebin

---
+++ StorageSystem/Controllers/ItemStorageBinController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using StorageSystem.Services;
+
+namespace StorageSystem.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ItemStorageBinController : ControllerBase
+    {
+
+
+        private readonly ItemStorageBinService _itemStorageBinService;
+
+
+        public ItemStorageBinController(ItemStorageBinService itemStorageBinService)
+        {
+            _itemStorageBinService = itemStorageBinService;
+        }
+    }
+}
