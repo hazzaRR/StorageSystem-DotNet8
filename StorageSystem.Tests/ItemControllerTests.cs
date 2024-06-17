5b1c054da8e62fba12e9042227622cdb84ccf4e8
harryredman06@gmail.com <68497855+hazzaRR@users.noreply.github.com>
2024-06-17 21:00:20 +0100

    added test for get all route for items

--- StorageSystem.Tests/ItemControllerTests.cs
+++ StorageSystem.Tests/ItemControllerTests.cs
@@ -1,6 +1,8 @@
-﻿using Moq;
+﻿using Microsoft.AspNetCore.Mvc;
+using Moq;
 using StorageSystem.Controllers;
 using StorageSystem.Interfaces;
+using StorageSystem.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -22,5 +24,63 @@ namespace StorageSystem.Tests
             _itemStorageBinService = new Mock<IItemStorageBinService>();
             _itemController = new ItemController(_itemService.Object, _itemStorageBinService.Object);
         }
+
+
+        [Fact]
+        public async void GetAllItems_ReturnsOk_AndList()
+        {
+            //Arrange
+
+            Location location = new Location
+            {
+                Id = 1,
+                Name = "Attic"
+            };
+
+            StorageBin bin = new StorageBin
+            {
+                Id = 1,
+                Location = location
+            };
+
+            List<Item> items = new List<Item>() {
+
+                new Item
+                {
+                    Id = 1,
+                    Name = "Zip Ties",
+                    Quantity = 4
+                },
+                new Item
+                {
+                    Id = 2,
+                    Name = "Spoon",
+                    Quantity = 1
+                }
+
+            };
+
+            _itemService.Setup(service => service.GetAll())
+                .ReturnsAsync(items);
+
+            //Act
+
+
+            var result = await _itemController.GetAll();
+
+
+            //Assert
+
+            Assert.IsType<OkObjectResult>(result);
+
+            var okResult = (OkObjectResult)result;
+
+            Assert.NotNull(okResult);
+
+            Assert.Equal(items, okResult.Value);
+
+        }
+
     }
+
 }
